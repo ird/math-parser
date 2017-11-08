fe5c0896e31cabb666cee642ecc109a660cb33c4
ird <31051194+ird@users.noreply.github.com>
2017-11-08 15:37:53 +0000

    added parse()

--- src/main/java/parse/Parser.java
+++ src/main/java/parse/Parser.java
@@ -4,9 +4,14 @@ public class Parser {
     LinkedList<Token> tokens;
     Token lookahead;
     public void parse(List<Token> tokens) {
-
+        this.tokens = tokens.clone();
+        lookahead = this.tokens.getFirst();
+        expression();
+        if(lookahead.token != Token.EPSILON) {
+            throw new ParserException("Unexpected token; " + lookahead);
+        }
     }
     public void expression() {
-        
+
     }
 }
